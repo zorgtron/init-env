518f1067c32669c8c89b06e9d3ba18a2b52a27ab
Andrew Miner <andrewminer@mac.com>
2017-07-10 13:55:10 -0700

    Make log trace clearer

--- install.sh
+++ install.sh
@@ -8,23 +8,23 @@ fi
 CONFIG_DIR=$PWD
 
 if ! which -s brew; then
-    echo "Brew is not installed. Installing now..."
+    echo; echo; echo "Brew is not installed. Installing now..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 pushd $HOME >/dev/null
 
-echo "Installing standard config files..."
+echo; echo; echo "Installing standard config files..."
 
 for FILE in $(ls -A "$CONFIG_DIR/home"); do
     [[ -e "$FILE" ]] && rm -rf "$FILE"
     ln -s "$CONFIG_DIR/home/$FILE" "$FILE"
 done
 
-echo "Updating brew, one moment..."
+echo; echo; echo "Updating brew, one moment..."
 brew update
 
-echo "Installing standard brew packages..."
+echo; echo; echo "Installing standard brew packages..."
 brew install fswatch
 brew install macvim
 brew install node
@@ -32,7 +32,7 @@ brew install rbenv
 brew install watch
 brew install wget
 
-echo "Installing standard NPM packages..."
+echo; echo; echo "Installing standard NPM packages..."
 npm install -g browserify
 npm install -g coffee-script
 npm install -g grunt-cli
@@ -40,9 +40,9 @@ npm install -g node-sass
 npm install -g nodemon
 npm install -g pug-cli
 
-read -p "Installing vi plugins, type \":qall\" when finished..."
+echo; echo; read -p "Installing vi plugins, type \":qall\" when finished..."
 vi -c ":PlugInstall"
 
 popd >/dev/null
-echo "Done."
+echo; echo; echo "Done."
 echo
